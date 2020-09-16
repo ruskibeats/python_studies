c30f0b7c0b124170777bc8f678a9605851b8e95a
Russell Batchelor <russellbatchelor@Russells-MacBook-Pro.local>
2020-09-16 16:46:09 +0100

    API KEY ADDED

--- Toolbox/HuntertoMYSQL.py
+++ Toolbox/HuntertoMYSQL.py
@@ -11,4 +11,8 @@ sqlEngine = create_engine("mysql+pymysql://{user}:{pw}@209.97.133.232/{db}"
                                   db="linkedin",
                                   CHARSET='utf8mb4'))
 
+""" API KEY """
+
+https: // api.hunter.io/v2/domain-search?domain = stripe.com & api_key = f68a4cb3f9c2ec8fac1bedd2ab9df424760404e3
+
 df.to_sql(name='hunter', con=sqlEngine, if_exists='append', index=False)
