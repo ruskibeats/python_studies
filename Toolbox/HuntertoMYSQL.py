21a7227b6f1bf4360e72a407cef8e70c172d2bd7
melvinlunder <melvinlunder@gmail.com>
2020-09-16 13:54:53 -0400

    Fetch data from Hunter and save them to MySql db

--- Toolbox/HuntertoMYSQL.py
+++ Toolbox/HuntertoMYSQL.py
@@ -1,18 +1,19 @@
 
 import pandas as pd
+import requests
 import sqlalchemy
 from sqlalchemy import create_engine
 
 import pymysql
-
 sqlEngine = create_engine("mysql+pymysql://{user}:{pw}@209.97.133.232/{db}"
                           .format(user="russ",
                                   pw="skimmer69skimmer",
                                   db="linkedin",
                                   CHARSET='utf8mb4'))
 
-""" API KEY """
-
-https: // api.hunter.io/v2/domain-search?domain = stripe.com & api_key = f68a4cb3f9c2ec8fac1bedd2ab9df424760404e3
-
+# """ API KEY """
+# https: // api.hunter.io/v2/domain-search?domain = stripe.com & api_key = f68a4cb3f9c2ec8fac1bedd2ab9df424760404e3
+contents = requests.get("https://api.hunter.io/v2/domain-search?domain=stripe.com&api_key=f68a4cb3f9c2ec8fac1bedd2ab9df424760404e3").json()
+df = pd.DataFrame(contents['data']['emails'])
+print(df)
 df.to_sql(name='hunter', con=sqlEngine, if_exists='append', index=False)
