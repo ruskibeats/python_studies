83a62d469e2b50c26e7795ca5bbbbf010b146eff
melvinlunder <melvinlunder@gmail.com>
2020-09-16 16:43:08 -0400

    flatten nested dictionary

--- Toolbox/HuntertoMYSQL.py
+++ Toolbox/HuntertoMYSQL.py
@@ -3,7 +3,7 @@ import pandas as pd
 import requests
 import sqlalchemy
 from sqlalchemy import create_engine
-
+import pprint
 import pymysql
 sqlEngine = create_engine("mysql+pymysql://{user}:{pw}@209.97.133.232/{db}"
                           .format(user="russ",
@@ -13,13 +13,17 @@ sqlEngine = create_engine("mysql+pymysql://{user}:{pw}@209.97.133.232/{db}"
 
 # """ API KEY """
 # https: // api.hunter.io/v2/domain-search?domain = stripe.com & api_key = f68a4cb3f9c2ec8fac1bedd2ab9df424760404e3
+
 contents = requests.get("https://api.hunter.io/v2/domain-search?domain=stripe.com&api_key=f68a4cb3f9c2ec8fac1bedd2ab9df424760404e3").json()
-data = {}
-data['data'] = str(contents['data'])
-data['meta'] = str(contents['meta'])
-df = pd.DataFrame(data, index=[0])
-try:
-        df.to_sql(name='hunter', con=sqlEngine, if_exists='append', index=False)
-except:
-        print("Cannot access MySQL Server")
-        print(sqlEngine)
+data = []
+for email in contents['data']['emails']:
+        for source in email['sources']:
+                record = email.copy()
+                del record['value']
+                del record['sources']
+                del record['verification']
+                record['email'] = email['value']
+                record.update(source)
+                data.append(record)
+df = pd.DataFrame(data)
+df.to_sql(name='hunter', con=sqlEngine, if_exists='append', index=False)
